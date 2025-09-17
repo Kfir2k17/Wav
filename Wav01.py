5ddd632bb967ef810cce6017a800d7c35094ad4d
Kfir Carmely <73598682+Kfir2k17@users.noreply.github.com>
2025-09-17 21:05:47 +0300

    Almost second exercsie

--- Wav01.py
+++ Wav01.py
@@ -1,43 +1,57 @@
 from scipy.io import wavfile
 
-def wav_to_list(filename): # Converts the files into a list of binary numbers
+def wav_to_list(filename, only_two=False): # Converts the files into a list of binary numbers
     rate, data = wavfile.read(filename)
 
     bin_list = []
     index = 0
 
+    chunk_num = 1 if only_two else 4
+
+    ZERO = 0 if only_two else - 3 # Setting the number that would represent zero
+
     for amplitude in data:
         list_length = len(bin_list)
 
-        if amplitude == 0 and index + 1 == list_length:
+        fixed_amplitude = int((amplitude / 1000) -3) if not only_two else amplitude
+
+        if fixed_amplitude == ZERO and index + 1 == list_length:
             index += 1
 
-        elif index == list_length:
+        elif index == list_length and only_two:
             if amplitude == 6000:
                 bin_list.append(0)
 
             elif amplitude == 16000:
                 bin_list.append(1)
 
-    return bin_list
+        elif index == list_length and not only_two:
+            bin_num = format(fixed_amplitude, 'b')
+            bin_str = bin_num.zfill(4)
+            bin_list.append(bin_str)
 
-def turn_list_to_string(ls): # Converts a list of binary numbers into text
+
+    return turn_list_to_string(bin_list, chunk_num)
+
+def turn_list_to_string(ls, chunks=1): # Converts a list of binary numbers into text
     list_length = len(ls)
 
     msg = ""
     byte = ""
 
+    divider = 8 / chunks
+
     for index in range(list_length):
         byte += str(ls[index])
 
-        if (index+1) % 8 == 0:
-            byte_converted = int(byte, 2)
+        if (index+1) % divider == 0:
+            byte_converted = int(byte, 2) if chunks == 1 else byte.encode()
             msg += chr(byte_converted)
             byte = ""
 
     return msg
 
+# message1 = wav_to_list('distress_signal_1.wav', True)
+message2 = wav_to_list('distress_signal_2.wav')
 
-file1 = wav_to_list('distress_signal_1.wav')
-message = (turn_list_to_string(file1))
-print(message)
+print(message2)
